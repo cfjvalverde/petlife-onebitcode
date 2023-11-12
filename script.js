e51b4e7e80b3f0f63323ba3b8b01bf4f46c63cc5
Valverde <78423491+cfjvalverde@users.noreply.github.com>
2023-11-12 16:05:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var elementosDuvidas = document.querySelectorAll('.duvidas')
+
+elementosDuvidas.forEach (function (duvidas) {
+    duvidas.addEventListener('click', function () {
+        duvidas.classList.toggle('ativa')
+    })
+
+})
+
+//function.nome(argumento){}
+
+
